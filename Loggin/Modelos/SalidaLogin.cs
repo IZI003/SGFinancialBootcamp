f01ab2bb7dcad3e41de0d373971991027b89b391
Ezequiel Miranda <76484376+IZI003@users.noreply.github.com>
2024-06-09 10:54:35 -0300

    Delete Loggin directory
    se elimina login se unifican proyectos

--- Loggin/Modelos/SalidaLogin.cs
+++
@@ -1,9 +0,0 @@
-﻿using Comunes.Respuesta;
-
-namespace Login.Modelos;
-
-public class SalidaLogin
-{
-    internal RespuestaBD respuestaBD { get; set; }
-    public UsuarioLogin usuario  { get;set;}
-}
--- Loggin/Servicios/ILogin.cs
+++
@@ -1,9 +0,0 @@
-﻿using Comunes.Respuesta;
-using Login.Modelos;
-
-namespace Login.Servicios;
-
-public interface ILogin
-{
-    Task<SalidaLogin> login(string usuario, string password);
-}
